85f4efcd0b95ade9e7607d1058ad0bbc66492025
vvelsone <velsonone@gmail.com>
2024-09-20 02:21:34 +0300

    upd9

--- if.h
+++ if.h
@@ -3,9 +3,50 @@ int if1(int a) {
     {
         a += 1;
     }
+    return a;
+}
+
+int if2(int a){ 
+    if(a >= 0)
+    {
+        a += 1;
+    }
     else
     {
-        return a;
+        a -= 2;
     }
     return a;
+}
+
+int if3(int a) {
+    if(a > 0)
+    {
+        a += 1;
+    }
+    if (a < 0)
+    {
+        a -= 2;
+    }
+    if (a == 0)
+    {
+        a += 10;
+    }
+    return a;
+}
+
+int if4(int a, int b, int c) {
+    int count = 0;
+    if(a > 0)
+    {
+        count += 1;
+    }
+    if(b > 0)
+    {
+        count += 1;
+    }
+    if(c > 0)
+    {
+        count += 1;
+    }
+    return count;
 }
--- main.cpp
+++ main.cpp
@@ -6,9 +6,7 @@
 
 int main()
 {
-  std::cout << "rezult: " << if1(5) << std::endl;
-
-
+  std::cout << "rezult: " << if4(1,2,0) << std::endl;
   // bool result = boolean::begin20(5, 5, 5);
   // std::cout << result << std::endl;
   return 0;
